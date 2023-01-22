53ef92fbc910f8c310ab5f17fa460acdbdb9fee0
Giorgi <mrmrela5@gmail.com>
2023-01-22 18:58:47 +0400

    fetch github users

--- Github Profiles/script.js
+++ Github Profiles/script.js
@@ -1,10 +1,24 @@
 'use strict';
 
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 const APIURL = 'https://api.github.com/users/';
 
 const getUser = async (username) => {
-  const res = await axios.get(APIURL + username);
-  console.log(res.data);
+  try {
+  const { data } = await axios.get(APIURL + username);
+  console.log( data );
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-getUser('mrelashvili');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = search.value;
+  if(username) {
+    getUser(username)
+
+    search.value = '';
+  }
+})
